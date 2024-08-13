60f8ff15772abb509a5161de9a18a9c915e7989e
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-08-12 22:06:45 -0500

    init commit

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBrrHywaG8i6zO2wHBP5Eq0S4klXLpmozs",
-  authDomain: "flashcards-455ed.firebaseapp.com",
-  projectId: "flashcards-455ed",
-  storageBucket: "flashcards-455ed.appspot.com",
-  messagingSenderId: "699512977938",
-  appId: "1:699512977938:web:7bc7f56dc2d6341f3f76cb",
-  measurementId: "G-3C8W1B1ZTE"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase

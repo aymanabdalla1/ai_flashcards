8302718e8d2a195ceb35ae9517650a38382bb171
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-08-14 11:13:54 -0500

    adding clerk

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import getStripe from "../utils/get-stripejs";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { Container } from "@mui/material";
+import { Container, Toolbar, Typography, AppBar, Button } from "@mui/material";
 
 export default function Home() {
   return (
@@ -10,6 +10,20 @@ export default function Home() {
         <title>Flashcard SaaS</title>
         <meta name="description" content="Create flashcards from your text with AI" />
       </Head>
+      <AppBar position = "static">
+        <Toolbar>
+          <Typography variant="h6" style={{flexGrow: 1}}>
+            Flashcard SaaS
+          </Typography>
+          <SignedOut>
+            <Button color="inherit"> Login </Button>
+            <Button color="inherit"> Sign Up </Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+        </AppBar>
     </Container>
   );
 }

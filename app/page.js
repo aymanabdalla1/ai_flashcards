81d84f97a552a8f909e986df78ec2b31df38330a
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-08-17 00:37:18 -0500

    adding handleSubmit to landing page

--- app/page.js
+++ app/page.js
@@ -17,6 +17,29 @@ import {
 } from "@mui/material";
 
 export default function Home() {
+  const handleSubmit = async () => {
+    const checkoutSession = await fetch('/api/checkout_session', {
+      method: 'POST',
+      headers: { origin: 'http://localhost:3000' },
+    });
+    const checkoutSessionJson = await checkoutSession.json();
+
+    if (checkoutSession.statusCode === 500) {
+      console.error(checkoutSession.message);
+      return;
+    }
+  
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
+      sessionId: checkoutSessionJson.id,
+    });
+  
+    if (error) {
+      console.warn(error.message);
+    }
+  }
+
+
   return (
     <Container
       maxWidth="100vw"
